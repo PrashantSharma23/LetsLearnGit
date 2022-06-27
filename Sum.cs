cd5df138e54db0b36c80c4bbc6b99d6fafe88b6c
prashant <prashant23sharma@gmail.com>
2022-06-27 10:23:58 +0530

    sum file commit

---
+++ Sum.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Sum
+{
+    public class Sum
+    {
+        static void Main(string[] args)
+        {
+            int a=3 ,b=2;
+            Console.WriteLine("Sum{0}=", a+b);
+            Console.ReadKey();
+        }
+        
+    }
+}
